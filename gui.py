97794c54ed272eba72453a8c19a3d521c80e5036
Niveditha Kovilakath <72668473+NivedithaKovilakath@users.noreply.github.com>
2022-04-13 14:07:50 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -15,10 +15,14 @@ connect()
 def submit():
     url = urlINP.get()
     loc = locINP.get()
-    if(radio.get()==1):
-        ip = iterativeDNSResolver(url,loc)
+    result=checkCache(url)
+    if(result=="empty"):
+        if(radio.get()==1):
+            ip = iterativeDNSResolver(url,loc)
+        else:
+            ip = recursiveDNSResolution(url,loc)
     else:
-        ip = recursiveDNSResolution(url,loc)
+        ip=getCache(url)
     selectLabel.config(text = "IP ADDRESS: "+ip)
 
 
